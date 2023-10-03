a71384c91b94bec8fd33608c5bea418385f49cf6
Rohit Nautiyal <rohitnautiyal2023@gmail.com>
2023-10-03 09:35:47 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,42 +1,20 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { ThemeProvider } from "./Component/theme";
-import ThemeBtn from "./Component/ThemeBtn";
-import Card from "./Component/Card";
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './Components/Login';
+import Profile from './Components/profile';
 
 function App() {
-  const [themeMode, setThemeMode] = useState("light");
-
-  const lightTheme = () => {
-    setThemeMode("light");
-  };
-  const darkTheme = () => {
-    setThemeMode("dark");
-  };
-  
-
-  useEffect(()=>{
-    document.querySelector('html').classList.remove("light","dark")
-    document.querySelector('html').classList.add(themeMode)
-  },[themeMode])
+ 
 
   return (
-    <>
-      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-        <div className="flex flex-wrap min-h-screen items-center">
-          <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-              <ThemeBtn />
-            </div>
-
-            <div className="w-full max-w-sm mx-auto">
-              <Card />
-            </div>
-          </div>
-        </div>
-      </ThemeProvider>
-    </>
-  );
+  <UserContextProvider>
+  <h2>Context API</h2>kk
+  <Login />
+  <Profile />
+  
+  </UserContextProvider>
+  )
 }
 
-export default App;
+export default App
